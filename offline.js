f71205245f2e7076fd37e4d82194803197060247
Kolya12342 <30775443+Kolya12342@users.noreply.github.com>
2019-01-06 19:56:35 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1546797433,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/topscoreboard-sheet0.png",
+		"images/avatarka_vk-sheet0.png",
+		"images/money_sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js",
+		"xd_connection.js"
+	]
+}
